b7ea06544de5cd372745690c347b6ba1997c280e
lijingpeng <2954015549@qq.com>
2023-07-08 10:57:15 +0800

    新增菜品业务代码开发，oos相关

---
+++ sky-server/src/main/java/com/sky/controller/admin/DishController.java
@@ -0,0 +1,46 @@
+package com.sky.controller.admin;
+
+import com.sky.dto.DishDTO;
+import com.sky.result.Result;
+import com.sky.service.DishService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@Api(tags = "菜品相关接口")
+@RequestMapping("/admin/dish")
+public class DishController {
+    @Autowired
+    DishService dishService;
+    @PostMapping
+    @ApiOperation("新增菜品")
+    public Result sava(@RequestBody DishDTO dishDTO){
+        log.info("新增菜品{}",dishDTO);
+        dishService.saveWithFlavor(dishDTO);
+        return  Result.success();
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ sky-server/src/main/java/com/sky/service/impl/DishServiceImpl.java
@@ -0,0 +1,61 @@
+package com.sky.service.impl;
+
+import com.sky.dto.DishDTO;
+import com.sky.entity.Dish;
+import com.sky.entity.DishFlavor;
+import com.sky.mapper.DishFlavorMapper;
+import com.sky.mapper.DishMapper;
+import com.sky.service.DishService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Slf4j
+public class DishServiceImpl implements DishService {
+    @Autowired
+    DishMapper dishMapper;
+    @Autowired
+    DishFlavorMapper dishFlavorMapper;
+
+    /**
+     * 新增菜品与对应的口味
+     * @param dishDTO
+     */
+    @Transactional
+    @Override
+    public void saveWithFlavor(DishDTO dishDTO) {
+        Dish dish=new Dish();
+        BeanUtils.copyProperties(dishDTO,dish);
+        //向菜品表插入数据
+        dishMapper.insert(dish);
+        //获取insert语句生成的主键值
+        Long dishId = dish.getId();
+
+        //向口味表插入n条数据
+        List<DishFlavor> flavors = dishDTO.getFlavors();
+        flavors.forEach(dishFlavor -> {
+            dishFlavor.setDishId(dishId);
+        });
+        if (flavors!=null&& flavors.size()>0){
+            dishFlavorMapper.insertBatch(flavors);
+        }
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
