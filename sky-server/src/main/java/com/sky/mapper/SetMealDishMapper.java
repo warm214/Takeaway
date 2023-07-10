3c8095cc785b67a63b5a74166fa01712b3984c1c
lijingpeng <2954015549@qq.com>
2023-07-10 13:35:46 +0800

    新增套餐部分

--- sky-server/src/main/java/com/sky/mapper/SetMealDishMapper.java
+++ sky-server/src/main/java/com/sky/mapper/SetMealDishMapper.java
@@ -1,5 +1,6 @@
 package com.sky.mapper;
 
+import com.sky.entity.SetmealDish;
 import org.apache.ibatis.annotations.Mapper;
 
 import java.util.List;
@@ -12,6 +13,11 @@ public interface SetMealDishMapper {
      * @return
      */
     List<Long> getSetMealIdsByDishIds(List<Long> dishIds);
+    /**
+     * 批量保存套餐和菜品的关联关系
+     * @param setmealDishes
+     */
+    void insertBatch(List<SetmealDish> setmealDishes);
 }
 
 
--- sky-server/src/main/java/com/sky/service/DishService.java
+++ sky-server/src/main/java/com/sky/service/DishService.java
@@ -2,6 +2,7 @@ package com.sky.service;
 
 import com.sky.dto.DishDTO;
 import com.sky.dto.DishPageQueryDTO;
+import com.sky.entity.Dish;
 import com.sky.result.PageResult;
 import com.sky.vo.DishVO;
 import org.springframework.stereotype.Service;
@@ -41,6 +42,13 @@ public interface DishService {
      * @param dishDTO
      */
     void updateDishWithFlavor(DishDTO dishDTO);
+
+    /**
+     * 根据分类id查询菜品
+     * @param categoryId
+     * @return
+     */
+    List<Dish> list(Long categoryId);
 }
 
 
